0442633f01670cc4e9cd24d6c29ff839678b2448
thi-coutinho <thi.coutinho.santos@gmail.com>
2022-11-25 15:06:48 -0300

    format document

--- src/components/Underlines.js
+++ src/components/Underlines.js
@@ -1,19 +1,19 @@
-export default function Underlines({palavra,letrasClicadas,statusJogo}) {
+export default function Underlines({ palavra, letrasClicadas, statusJogo }) {
     let letrasUnderlines
-    const palavraString = palavra.toString().replaceAll(",","")
-    if (statusJogo==="prejogo") {
+    const palavraString = palavra.toString().replaceAll(",", "")
+    if (statusJogo === "prejogo") {
         return <></>
-    } else if (statusJogo==="jogando"){
+    } else if (statusJogo === "jogando") {
 
         letrasUnderlines = palavra.map((l) => (
-            letrasClicadas.includes(l.normalize('NFD').replace(/[\u0300-\u036f]/g, '')) ? l:" _ "))
-        letrasUnderlines=letrasUnderlines.toString().replaceAll(",","")
+            letrasClicadas.includes(l.normalize('NFD').replace(/[\u0300-\u036f]/g, '')) ? l : " _ "))
+        letrasUnderlines = letrasUnderlines.toString().replaceAll(",", "")
     } else {
         letrasUnderlines = palavraString
     }
 
 
     return (
-        <div data-test="word" data-answer={statusJogo!=="prejogo" ? `${palavraString}` : ""} className={`underline ${statusJogo}`}>{letrasUnderlines}</div>
+        <div data-test="word" data-answer={statusJogo !== "prejogo" ? `${palavraString}` : ""} className={`underline ${statusJogo}`}>{letrasUnderlines}</div>
     )
 }
